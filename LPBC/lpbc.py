4e873b7e9bea8db51c7fe7c70b618a1e085b64b1
Leocchu <51972137+Leocchu@users.noreply.github.com>
2019-08-01 18:58:46 -0700

    Update lpbc.py

--- LPBC/lpbc.py
+++ LPBC/lpbc.py
@@ -59,7 +59,7 @@ class democotroller(pbc.LPBCProcess):
         self.batt_cmd = np.array([])
         self.mode = 0  # mode 1: PV as disturbance, mode 2: PV calculated, mode 3: PV only
         self.p_ctrl = np.array([])
-        self.group_id = [0,1,2] # TODO: group id for motors corresponding to phase: Must be in ascending order
+        self.group_id = [0,1,2] # TODO: group id for motors corresponding to phase: Must be in ascending order. CHANGE CHANNELS IN TOML!
         self.local_s_ratio = 500/3.3 #TODO: Inverters should have ratio of 500/3.3 ; Load racks : 250
         self.Pcmd_inv = np.array([])
         self.Qcmd_inv = np.array([])
